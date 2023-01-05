37f62bbf855cedebd2fcbf01b4c58454126f4211
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-01-06 00:58:46 +0530

    create route for user login

--- routes/user.js
+++ routes/user.js
@@ -16,3 +16,16 @@ router.post('/users', async (req, res) => {
     res.status(400).send(err);
   }
 });
+
+// login
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+
+    res.send({ user, token });
+  }
+  catch (err) {
+    res.status(400).send(err);
+  }
+});
