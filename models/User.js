0d3a8b74deed79f526bf4623101837cfd01786f7
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-01-06 00:51:57 +0530

    user authentication in User model
    generate JWT, hash password, find user (match email) and match password

--- models/User.js
+++ models/User.js
@@ -45,5 +45,37 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// generate auth token (jwt)
+userSchema.methods.generateAuthToken = async () => {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  user.tokens = user.tokens.concat({token});
+  await user.save();
+
+  return token;
+};
+
+// hash plain-text password
+userSchema.pre('save', async (cb_func) => {
+  const user = this;
+
+  if (user.isModified('password'))
+    user.password = await bcrypt.hash(user.password, 8);
+
+  cb_func(); // call back function
+});
+
+// fetch user
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User not registered.");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error("Password does not match.");
+
+  return user;
+}
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
