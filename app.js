0904b76f39c0a22551d64ad5b13ed6d6df68c09d
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-01-30 14:52:57 +0530

    link routes in app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const userRouter = require('./routes/user');
+const itemRouter = require('./routes/item');
+const cartRouter = require('./routes/cart');
 require('./db/mongoose');
 
-const app = express();
-app.use(express.json());
-
 const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(itemRouter);
+app.use(cartRouter);
 
 app.listen(port, () => {
   console.log("listening on port " + port + " ...");
