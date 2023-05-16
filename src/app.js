12e8b648333607b3ef3b147d90e590e25266ebc9
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-05-16 11:02:49 +0530

    create order route
    add post route for creating orders, app use order route in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const userRouter = require('./routes/user');
 const itemRouter = require('./routes/item');
 const cartRouter = require('./routes/cart');
+const orderRouter = require('./routes/order');
+
 require('./db/mongoose');
 
 const port = process.env.PORT;
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(userRouter);
 app.use(itemRouter);
 app.use(cartRouter);
+app.use(orderRouter);
 
 app.listen(port, () => {
   console.log("listening on port " + port + " ...");
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Order = require('../models/Order');
+const User = require('../models/User');
+const Cart = require('../models/Cart');
 const Auth = require('../middleware/auth');
 
 const router = new express.Router();
@@ -16,3 +18,33 @@ router.get('/orders', Auth, async (req, res) => {
     res.status(500).send();
   }
 });
+
+// place order
+router.post('/orders', Auth, async (req, res) => {
+  const owner = req.user._id;
+  try {
+    // find cart
+    const cart = await Cart.findOne({ owner });
+
+    if (cart && cart.items.length > 0) {
+      const newOrder = await Order.create({
+        owner,
+        items: cart.items,
+        bill: cart.bill
+      });
+
+      return res.status(201).send(newOrder);
+    }
+    else
+      return res.status(404).send({
+        message: "No items in cart"
+      });
+
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send("something went wrong     :(");
+  }
+});
+
+module.exports = router;
